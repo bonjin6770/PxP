0565a2677869714a1787c7086f25046c6cff1415
bonjin6770 <bonjin6770@gmail.com>
2016-09-06 01:56:18 +0900

    Add Domain Classes

---
+++ Library.PxP/GenerateAllPairsService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.PxP
+{
+    public class GenerateAllPairsService
+    {
+        public Pairs Execute(Parameters parameters)
+        {
+            return new Pairs();
+        }
+    }
+}
---
+++ Library.PxP/Values.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.PxP
+{
+    public class Values
+    {
+        public List<IValues> ToList { get; private set; }
+    }
+}
